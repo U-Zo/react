aca36beff6f4ee900066f38329af60dcbdee5d19
u_zo <kwoojo0127@naver.com>
2020-01-13 09:24:27 +0900

    Add styling react practice app
    Commit before eject

---
+++ styling-react/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SassComponent from "./SassComponent";
+
+const App = () => {
+    return (
+        <SassComponent/>
+    );
+};
+
+export default App;

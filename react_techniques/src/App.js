5052f093318d43a0e3ad32a7cc60575eb1580ca0
u_zo <kwoojo0127@naver.com>
2020-01-21 20:24:35 +0900

    Add react book contents

---
+++ react_techniques/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ValidationSample from "./ValidationSample";
+
+function App() {
+    return (
+        <div>
+          <ValidationSample></ValidationSample>
+        </div>
+    );
+}
+
+export default App;

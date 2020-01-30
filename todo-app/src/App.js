5b307312bac3added4b678d5b6417c074085bcfd
u_zo <kwoojo0127@naver.com>
2020-01-30 21:54:10 +0900

    Update todo-app
    Optimize todo-app using react-virtualized

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,4 +1,4 @@
-import React, {useCallback, useRef, useState} from 'react';
+import React, {useCallback, useRef, useReducer} from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from "./components/TodoList";
@@ -16,8 +16,22 @@ function createBulkTodos() {
     return array;
 }
 
+function todoReducer(todos, action) {
+    switch (action.type) {
+        case 'INSERT':
+            return todos.concat(action.todo);
+        case 'REMOVE':
+            return todos.filter(todo => todo.id !== action.id);
+        case 'TOGGLE':
+            return todos.map(todo =>
+            todo.id === action.id ? {...todo, checked: !todo.checked} : todo);
+        default:
+            return todos;
+    }
+}
+
 const App = () => {
-    const [todos, setTodos] = useState(createBulkTodos);
+    const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
 
     // 고유값으로 사용될 id
     // ref를 사용하여 변수 담기
@@ -29,25 +43,21 @@ const App = () => {
                 text,
                 checked: false
             };
-            setTodos(todos => todos.concat(todo));
+            dispatch({type: 'INSERT', todo});
             nextId.current += 1; // nextId 1씩 더함
-        },
-        [todos]
+        }, []
     );
 
     const onRemove = useCallback(
         id => {
-            setTodos(todos => todos.filter(todo => todo.id !== id));
-        },
-        [todos]
+            dispatch({type: 'REMOVE', id});
+        }, []
     );
 
     const onToggle = useCallback(
         id => {
-            setTodos(
-                todos => todos.map(todo => todo.id === id ? {...todo, checked: !todo.checked} : todo)
-            );
-        }, [todos]
+            dispatch({type: 'TOGGLE', id});
+        }, []
     );
 
     return (

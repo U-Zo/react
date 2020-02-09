5e9098967311a1032a85be01c2278b0e5985d023
u_zo <kwoojo0127@naver.com>
2020-02-09 20:03:43 +0900

    Update ColorBox.js
    Use useContext in the component type function

--- context-tutorial/src/components/ColorBox.js
+++ context-tutorial/src/components/ColorBox.js
@@ -1,28 +1,25 @@
-import React from "react";
-import {ColorConsumer} from "../contexts/color";
+import React, {useContext} from "react";
+import ColorContext, {ColorConsumer} from "../contexts/color";
 
 const ColorBox = () => {
+    const {state} = useContext(ColorContext); // 함수형 컴포넌트에만 사용 가능
+
     return (
-        <ColorConsumer>
-            {({state}) => (
-                <>
-                    <div
-                        style={{
-                            width: '64px',
-                            height: '64px',
-                            background: state.color
-                        }}
-                    />
-                    <div
-                        style={{
-                            width: '32px',
-                            height: '32px',
-                            background: state.subcolor
-                        }}
-                    />
-                </>
-            )}
-        </ColorConsumer>
+        <>
+            <div style={{
+                width: '64px',
+                height: '64px',
+                background: state.color
+            }}
+            />
+            <div
+                style={{
+                    width: '32px',
+                    height: '32px',
+                    background: state.subcolor
+                }}
+            />
+        </>
     );
 };
 

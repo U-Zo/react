9ba6bcefe8eb51b20f3dab5256696a7f89397c49
u_zo <kwoojo0127@naver.com>
2020-02-11 01:17:28 +0900

    Update counter.js, todos.js
    use redux-actions

--- react-redux-tutorial/src/modules/counter.js
+++ react-redux-tutorial/src/modules/counter.js
@@ -1,4 +1,5 @@
 // 초기 상태 및 리듀서
+import {createAction, handleActions} from 'redux-actions';
 
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
@@ -10,19 +11,11 @@ const initialState = {
     number: 0
 };
 
-function counter(state = initialState, action) {
-    switch (action.type) {
-        case INCREASE:
-            return {
-                number: state.number + 1
-            };
-        case DECREASE:
-            return {
-                number: state.number - 1
-            };
-        default:
-            return state;
-    }
-}
+const counter = handleActions(
+    {
+        [INCREASE]: (state, action) => ({number: state.number + 1}),
+        [DECREASE]: (state, action) => ({number: state.number - 1})
+    }, initialState
+);
 
 export default counter;
--- react-redux-tutorial/src/modules/todos.js
+++ react-redux-tutorial/src/modules/todos.js
@@ -1,14 +1,19 @@
+import {createAction, handleActions} from 'redux-actions';
+
 const CHANGE_INPUT = 'todos/CHANGE_INPUT'; // 인풋 값 변경
 const INSERT = 'todos/INSERT'; // 새로운 todo 등록
 const TOGGLE = 'todos/TOGGLE'; // todo 체크 / 체크 해제
 const REMOVE = 'todos/REMOVE'; // todo 제거
 
+let id = 3;
+/*
 export const changeInput = input => ({
     type: CHANGE_INPUT,
     input
 });
 
-let id = 3;
+
+
 export const insert = text => ({
     type: INSERT,
     todo: {
@@ -17,6 +22,7 @@ export const insert = text => ({
         done: false
     }
 });
+
 
 export const toggle = id => ({
     type: TOGGLE,
@@ -27,6 +33,19 @@ export const remove = id => ({
     type: REMOVE,
     id
 });
+ */
+
+export const changeInput = createAction(CHANGE_INPUT, input => input);
+
+export const insert = createAction(INSERT, text => ({
+    id: id++,
+    text,
+    done: false
+}));
+
+export const toggle = createAction(TOGGLE, id => id);
+
+export const remove = createAction(REMOVE, id => id);
 
 const initialState = {
     input: '',
@@ -44,6 +63,7 @@ const initialState = {
     ]
 };
 
+/*
 function todos(state = initialState, action) {
     switch (action.type) {
         case CHANGE_INPUT:
@@ -72,5 +92,26 @@ function todos(state = initialState, action) {
             return state;
     }
 }
+*/
+
+const todos = handleActions(
+    {
+        [CHANGE_INPUT]: (state, {payload: input}) => ({...state, input: input}),
+        [INSERT]: (state, {payload: todo}) => ({
+            ...state,
+            todos: state.todos.concat(todo)
+        }),
+        [TOGGLE]: (state, {payload: id}) => ({
+            ...state,
+            todos: state.todos.map(todo =>
+                todo.id === id ? {...todo, done: !todo.done} : todo
+            )
+        }),
+        [REMOVE]: (state, {payload: id}) => ({
+            ...state,
+            todos: state.todos.filter(todo => todo.id !== id)
+        })
+    }, initialState
+);
 
 export default todos;

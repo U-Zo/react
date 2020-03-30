9b1d48a2f6b4ced3f8f081afc2cb543a44a128a7
u_zo <kwoojo0127@naver.com>
2020-03-30 20:44:07 +0900

    Update back-end
    Complete back-end developing, Commit before start front-end

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -4,13 +4,26 @@ import Joi from 'joi'; // Validation plugin
 
 const {ObjectId} = mongoose.Types;
 
-export const checkObjectId = (ctx, next) => {
+export const getPostById = async (ctx, next) => {
     const {id} = ctx.params;
     if (!ObjectId.isValid(id)){
         ctx.status = 400; // Bad Request
         return;
     }
-    return next();
+
+    try {
+        const post = await Post.findById(id);
+        // 포스트가 존재하지 않을 때
+        if (!post) {
+            ctx.status = 404; // Not Found
+            return;
+        }
+
+        ctx.state.post = post;
+        return next();
+    } catch (e) {
+        ctx.throw(500, e);
+    }
 };
 
 export const write = async ctx => {
@@ -35,7 +48,8 @@ export const write = async ctx => {
     const post = new Post({
         title,
         body,
-        tags
+        tags,
+        user: ctx.state.user
     });
     try {
         await post.save();
@@ -55,14 +69,21 @@ export const list = async ctx => {
         return;
     }
 
+    const {tag, username} = ctx.query;
+    // tag, username 값이 유효하면 객체 안에 넣고 그렇지 않으면 넣지 않음
+    const query = {
+        ...(username ? {'user.username': username} : {}),
+        ...(tag ? {tags: tag} : {})
+    };
+
     try {
-        const posts = await Post.find()
+        const posts = await Post.find(query)
             .sort({_id: -1}) // 1: 오름차순, -1: 내림차순
             .limit(10)
             .skip((page - 1) * 10)
             .lean()
             .exec();
-        const postCount = await Post.countDocuments().exec();
+        const postCount = await Post.countDocuments(query).exec();
         ctx.set('Last-Page', Math.ceil(postCount / 10)); // 마지막 페이지 설정
         ctx.body = posts
             //.map(post => post.toJSON())
@@ -76,17 +97,7 @@ export const list = async ctx => {
 };
 
 export const read = async ctx => {
-    const {id} = ctx.params;
-    try {
-        const post = await Post.findById(id).exec();
-        if (!post) {
-            ctx.status = 404; // Not Found
-            return;
-        }
-        ctx.body = post
-    } catch (e) {
-        ctx.throw(500, e);
-    }
+    ctx.body = ctx.state.post;
 };
 
 export const remove = async ctx => {
@@ -129,4 +140,13 @@ export const update = async ctx => {
     } catch (e) {
         ctx.throw(500, e);
     }
+};
+
+export const checkOwnPost = (ctx, next) => {
+    const {user, post} = ctx.state;
+    if (post.user._id.toString() !== user._id) {
+        ctx.status = 403;
+        return;
+    }
+    return next();
 };

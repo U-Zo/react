ff17ffaaee9f0c82b6ce4eace9e38aac91dbd9df
u_zo <kwoojo0127@naver.com>
2020-03-30 19:25:10 +0900

    Update auth.ctrl.js
    Add Login function

--- blog/blog-backend/src/api/auth/auth.ctrl.js
+++ blog/blog-backend/src/api/auth/auth.ctrl.js
@@ -3,7 +3,7 @@ import User from '../../models/user';
 
 export const register = async ctx => {
     // Request Body 검증하기
-    const schema = Joi.Object().keys({
+    const schema = Joi.object().keys({
         username: Joi.string()
             .alphanum()
             .min(3)
@@ -42,7 +42,33 @@ export const register = async ctx => {
 };
 
 export const login = async ctx => {
-    // 로그인
+    const {username, password} = ctx.request.body;
+
+    // username, password가 없으면 에러 처리
+    if (!username || !password) {
+        ctx.status = 401; // Unauthorized
+        return;
+    }
+
+    try {
+        const user = await User.findByUsername(username);
+        // 계정이 존재하지 않으면 에러 처리
+        if (!user) {
+            ctx.status = 401;
+            return;
+        }
+
+        const valid = await user.checkPassword(password);
+        // 잘못된 비밀번호
+        if (!valid) {
+            ctx.status = 401;
+            return;
+        }
+
+        ctx.body = user.serialize();
+    } catch (e) {
+        ctx.throw(500, e);
+    }
 };
 
 export const check = async ctx => {

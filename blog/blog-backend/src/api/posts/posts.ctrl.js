0cf6566c355d79980f3842644f9280d35ea15d7e
u_zo <kwoojo0127@naver.com>
2020-03-30 18:11:10 +0900

    Add Joi
    Commit before add pagination

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -1,5 +1,6 @@
 import Post from '../../models/post';
 import mongoose from 'mongoose';
+import Joi from 'joi'; // Validation plugin
 
 const {ObjectId} = mongoose.Types;
 
@@ -13,6 +14,23 @@ export const checkObjectId = (ctx, next) => {
 };
 
 export const write = async ctx => {
+    const schema = Joi.object().keys({
+        // 객체가 다음 필드를 가지고 있을음 검증
+        title: Joi.string().required(), // required()가 있으면 필수 항목
+        body: Joi.string().required(),
+        tags: Joi.array()
+            .items(Joi.string())
+            .required() // 문자열로 이루어진 배열
+    });
+
+    // 검증하고 나서 검증 실패인 경우 에러 처리
+    const result = Joi.validate(ctx.request.body, schema);
+    if (result.error) {
+        ctx.status = 400; // Bad Request
+        ctx.body = result.error;
+        return;
+    }
+
     const {title, body, tags} = ctx.request.body;
     const post = new Post({
         title,
@@ -62,6 +80,21 @@ export const remove = async ctx => {
 
 export const update = async ctx => {
     const {id} = ctx.params;
+    // required 없이 작성
+    const schema = Joi.object().keys({
+        title: Joi.string(),
+        body: Joi.string(),
+        tags: Joi.array().items(Joi.string())
+    });
+
+    // 검증하고 나서 검증 실패인 경우 에러 처리
+    const result = Joi.validate(ctx.request.body, schema);
+    if (result.error) {
+        ctx.status = 400; // Bad request
+        ctx.body = result.error;
+        return;
+    }
+
     try {
         const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
             new: true // 이 값을 설정하면 업데이트된 데이터 반환

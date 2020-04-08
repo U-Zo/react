6664e84975fdfe9e488bffa0e1345e943ff82e46
u_zo <kwoojo0127@naver.com>
2020-04-08 13:26:20 +0900

    Update Button.js, Header.js, Responsive.js
    Commit before update Button.js routing that change component withRouter to Link

--- blog/blog-frontend/src/components/common/Responsive.js
+++ blog/blog-frontend/src/components/common/Responsive.js
@@ -4,9 +4,9 @@ import styled from 'styled-components';
 const ResponsiveBlock = styled.div`
   padding-left: 1rem;
   padding-right: 1rem;
-  width: 1024px;
+  width: 100%;
   margin: 0 auto;
-  
+  /*
   @media (max-width: 1024px) {
     width: 768px;
   }
@@ -14,6 +14,7 @@ const ResponsiveBlock = styled.div`
   @media (max-width: 768px) {
     width: 100%;
   }
+  */
 `;
 
 const Responsive = ({children, ...rest}) => {

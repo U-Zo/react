1c2bf7426a3dc961121814dcced2ed6253dcfc3d
u_zo <kwoojo0127@naver.com>
2020-08-16 17:01:17 +0900

    Add react context for manage states

--- toy/todo/src/App.js
+++ toy/todo/src/App.js
@@ -1,10 +1,6 @@
-import React, {useRef, useState} from 'react';
-import {createGlobalStyle} from 'styled-components';
-import TodoTemplate from './components/TodoTemplate';
-import TodoHead from './components/TodoHead';
-import TodoList from './components/TodoList';
-import TodoCreate from './components/TodoCreate';
-import {TodoProvider} from './modules/TodoContext';
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import TodoPage from './pages/TodoPage';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -13,62 +9,12 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 const App = () => {
-    const [todos, setTodos] = useState([
-        {
-            id: 1,
-            text: '프로젝트 생성하기',
-            done: true
-        },
-        {
-            id: 2,
-            text: '컴포넌트 스타일링하기',
-            done: true
-        },
-        {
-            id: 3,
-            text: 'Context 만들기',
-            done: false
-        },
-        {
-            id: 4,
-            text: '기능 구현하기',
-            done: false
-        }
-    ]);
-
-    let nextId = 5;
-
-    const onInsert = (text) => {
-        setTodos([
-            ...todos,
-            {
-                id: nextId,
-                text: text,
-                done: false
-            }
-        ]);
-
-        nextId++;
-    };
-
-    const onEdit = (id, text) => {
-        let newTodos = [...todos];
-        newTodos[id - 1] = {
-            text
-        };
-        setTodos(newTodos);
-    };
-
     return (
-        <TodoProvider>
+        <>
             <GlobalStyle/>
-            <TodoTemplate>
-                <TodoHead/>
-                <TodoList todos={todos} onEdit={onEdit}/>
-                <TodoCreate onInsert={onInsert}/>
-            </TodoTemplate>
-        </TodoProvider>
+            <TodoPage/>
+        </>
     );
-}
+};
 
 export default App;
